6b4533d9cd10577e2f141f84128edb8abcf3f3c8
Pontus <pontus.abrahamsson@iths.se>
2024-07-24 15:18:05 +0200

    chore: Remove unused code and refactor Dialog component

--- frontend/src/components/reuseable/Dialog.tsx
+++ frontend/src/components/reuseable/Dialog.tsx
@@ -1,9 +1,5 @@
-import Button from "@mui/material/Button";
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
-import Typography from "@mui/material/Typography";
-import { useState } from "react";
-import { Box } from "@mui/material";
 
 export interface SimpleDialogProps {
   open: boolean;
