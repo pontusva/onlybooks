94b8b3e32237f1ee2e1ba1b5b57e3a4f78bb4516
Pontus <pontus.abrahamsson@iths.se>
2024-08-16 18:42:11 +0200

    Audio player styling

--- frontend/src/components/streaming/HLSplayer.tsx
+++ frontend/src/components/streaming/HLSplayer.tsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
+import { Box, Slider, Button, Typography } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import StopIcon from "@mui/icons-material/Stop";
+import { formatTime } from "../../utils";
 import Hls from "hls.js";
 
 interface HLSPlayerProps {
@@ -9,10 +14,11 @@ interface HLSPlayerProps {
 const HLSPlayer: React.FC<HLSPlayerProps> = ({ folder, filename }) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   useEffect(() => {
     const audio = audioRef.current;
-
     if (audio) {
       if (Hls.isSupported() && folder && filename) {
         const hls = new Hls();
@@ -26,7 +32,6 @@ const HLSPlayer: React.FC<HLSPlayerProps> = ({ folder, filename }) => {
       } else if (audio.canPlayType("application/vnd.apple.mpegurl")) {
         audio.src = `http://localhost:8888/playlist/${folder}/${filename}.m3u8`;
         audio.addEventListener("loadedmetadata", () => {});
-
         return () => {
           audio.removeEventListener("loadedmetadata", () => {});
         };
@@ -47,10 +52,60 @@ const HLSPlayer: React.FC<HLSPlayerProps> = ({ folder, filename }) => {
     }
   };
 
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    if (audio) {
+      const updateTime = () => setCurrentTime(audio.currentTime);
+      const setAudioDuration = () => setDuration(audio.duration);
+
+      audio.addEventListener("timeupdate", updateTime);
+
+      audio.addEventListener("loadedmetadata", setAudioDuration);
+
+      return () => {
+        audio.removeEventListener("timeupdate", updateTime);
+        audio.removeEventListener("loadedmetadata", setAudioDuration);
+      };
+    }
+  }, []);
+
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    const audio = audioRef.current;
+    const newTime = ((newValue as number) / 100) * duration;
+    if (audio) {
+      setDuration(audio.duration);
+      audio.currentTime = newTime;
+      setCurrentTime(newTime);
+    }
+  };
+
   return (
     <div>
-      <audio ref={audioRef} controls style={{ width: "100%" }} />
-      <button onClick={handlePlayPause}>{isPlaying ? "Pause" : "Play"}</button>
+      <Box sx={{ width: 300 }}>
+        <audio ref={audioRef} style={{ display: "none" }} />
+        <Box sx={{ width: "100%", display: "flex", alignItems: "center" }}>
+          <Button onClick={handlePlayPause}>
+            {!isPlaying ? <PlayCircleIcon /> : <StopIcon />}
+          </Button>
+          <Slider
+            value={(currentTime / duration) * 100}
+            onChange={handleSliderChange}
+            sx={{
+              height: 8,
+              "& .MuiSlider-thumb": { bgcolor: "blue", width: 12, height: 12 },
+              "& .MuiSlider-track": { bgcolor: "blue" },
+              "& .MuiSlider-rail": { bgcolor: "lightgrey" },
+            }}
+          />
+          <Box sx={{ ml: 2, display: "flex", alignItems: "center" }}>
+            <AccessTimeIcon sx={{ color: "text.secondary", mr: 0.5 }} />
+            <Typography variant="body2" color="textSecondary">
+              {formatTime(currentTime)}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 };
